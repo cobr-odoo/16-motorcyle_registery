99cc6819f73a1391335406fed41da6fc62ac3c1d
cobr-odoo <cobr@odoo.com>
2023-06-15 13:10:42 -0400

    first commit

---
+++ __manifest__.py
@@ -0,0 +1,22 @@
+{
+	'name': 'Motorcycle Registry',
+	'summary': 'Manage Registration of Motorcycles',
+	'description': """Motorcycle Registry
+			====================
+			This Module is used to keep track of the Motorcycle
+			 Registration and Ownership of each motorcycled
+			 of the brand.""",
+	'license': 'OPL-1',
+	'author': 'cobr-odoo',
+	'website': '16-motorcylce_registry',
+	'category': 'Kawill/',
+	'depends': ['base'],
+        'data': [
+            'security/motorcycle_groups.xml',
+            'security/ir.model.access.csv','security/motorcycle_security.xml'
+		],
+	'demo': [
+		'demo/motorcycle_demo.xml'
+	],
+        'application': True,
+}
